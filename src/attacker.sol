df09fcc67e1756d495ba52fafb39d485d90869ae
Jayakumar2812 <jayakumargowtham2812@gmail.com>
2025-01-20 15:44:18 +0530

    first commit

--- src/attacker.sol
+++ src/attacker.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "forge-std/console.sol";
 
 interface IReplica {
